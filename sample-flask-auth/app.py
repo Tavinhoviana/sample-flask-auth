20c471de400bd2c5b0a7b2439130f12b19224350
Your Name <you@example.com>
2025-04-30 14:33:33 -0300

    updating

---
+++ sample-flask-auth/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+@app.route("/", methods=["GET"])
+def hello_world():
+    return "Beautiful"
+
+if __name__ == "__main__":
+    app.run(debug=True)
